492cb0b67036300a64392e4d67c4bdca8db1b386
Gyenna <gyenna73@gmail.com>
2022-02-20 22:43:14 +0400

    --

--- Lesson 1-1.py
+++ Lesson 1-1.py
@@ -5,4 +5,5 @@ elif duration < 3600:
     print(duration // 60, " мин ", duration % 60, " сек")
 elif duration < 86400:
     print(duration // 3600, " час ", duration % 3600 // 60, " мин ", duration % 3600 % 60, " сек")
-else:  print(duration // 86400, " дн ", duration % 86400 // 3600, " час ", duration % 86400 % 3600 // 60, " мин ", duration % 3600 % 60, " сек")
+else:
+    print(duration // 86400, " дн ", duration % 86400 // 3600, " час ", duration % 86400 % 3600 // 60, " мин ", duration % 3600 % 60, " сек")
